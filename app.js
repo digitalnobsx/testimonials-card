81ec4f79cc4bbbfd52e71502f9091af3fb9e72fc
digitalnobsx <josephitoro23@gmail.com>
2022-11-18 09:13:36 +0100

    select items||set starting item

--- app.js
+++ app.js
@@ -38,3 +38,20 @@ const reviews = [
   },
 ];
 
+//Select items
+
+const img = document.getElementById("person-img");
+const name = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// Set starting item
+
+let currentItem = 0;
+
+//load initial item
